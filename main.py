ea9a0eaa178a6a11329c4d27fd93fcf0caa11273
amerilain <amerilain@gmail.com>
2022-08-25 14:41:42 +0300

    First assignment phase 1 and 2

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+import random
+"""
+# phase 1
+name = input("What is Your name? ")
+print("hello, " + name + "!")
+
+# phase 2
+radius = float(input("What is the radius of the circle? "))
+print(f"The area of the circle is {math.pi * (radius ** 2):.2f}")
+"""
+print(f"{random.randint(0, 999):03d}")
+print(str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)))
