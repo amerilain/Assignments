7a78200abd6f0f442a0233e176f39d1094ff59bf
amerilain <amerilain@gmail.com>
2022-08-29 14:42:35 +0300

    beginning of second week's exercises and practice

--- Week2.py
+++ Week2.py
@@ -1,6 +1,7 @@
 # Write a program that asks the user how much money they have and lets them know if they have enough
 # for a latte that costs 5 euros.
 # If they don't have enough, there is no output.
+#
 
 money = float(input("Enter amount of money: "))
 if money >= 5:
