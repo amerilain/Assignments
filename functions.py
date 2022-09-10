9b7c7e66c19cafb35ee18fe80aede786ec4c0dac
amerilain <amerilain@gmail.com>
2022-09-10 12:48:26 +0300

    first draft

---
+++ functions.py
@@ -0,0 +1,4 @@
+# part 1
+# Write a function that returns a random dice roll between 1 and 6. The function should not have
+# any parameters. Write a main program that rolls the dice until the result is 6. The main program
+# should print out the result of each roll.
