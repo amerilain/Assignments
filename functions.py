355da0c7abc6b31cc508ad75cf58b177dfa85db4
amerilain <amerilain@gmail.com>
2022-09-10 13:08:40 +0300

    part 1 done

--- functions.py
+++ functions.py
@@ -1,4 +1,18 @@
+import random
+
 # part 1
 # Write a function that returns a random dice roll between 1 and 6. The function should not have
 # any parameters. Write a main program that rolls the dice until the result is 6. The main program
 # should print out the result of each roll.
+
+dice = 0
+
+
+def roll_dice():
+    return random.randint(1,6)
+
+
+while dice != 6:
+    dice = roll_dice()
+    print(f"dice number is {dice}")
+
