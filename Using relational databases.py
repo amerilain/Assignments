d110e3356b9098b9d4c2701e9f1665f332288806
amerilain <amerilain@gmail.com>
2022-09-24 10:44:29 +0300

    parts 1 and 2 complete

--- Using relational databases.py
+++ Using relational databases.py
@@ -9,6 +9,7 @@ connection = mariadb.connect(
          autocommit=True
          )
 
+# Part 1
 icao = input("Enter IACO: ")
 sql = "SELECT name, municipality FROM airport WHERE ident = '"+icao+"'"
 cursor = connection.cursor()
@@ -17,3 +18,15 @@ response = cursor.fetchall()
 if cursor.rowcount >0:
     for row in response:
         print(f"Airport: {row[0]}\nLocation: {row[1]} ")
+
+# Write a program that asks the user to enter the area code (for example FI) and prints out the airports located
+# in that country ordered by airport type. For example, Finland has 65 small airports, 15 helicopter airports and so on.
+code = input("Enter country code: ")
+sql = "SELECT airport.name AS 'Airport Name', type AS 'Airport Type' FROM airport, country WHERE " \
+      "airport.iso_country=country.iso_country and country.iso_country= '"+code+"' ORDER BY type asc; "
+cursor = connection.cursor()
+cursor.execute(sql)
+response = cursor.fetchall()
+if cursor.rowcount >0:
+    for row in response:
+        print(f"{row[0]}, {row[1]}")
