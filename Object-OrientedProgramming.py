dd5e2288526f05623eedc3856b73834481c80f6a
amerilain <amerilain@gmail.com>
2022-10-21 14:51:25 +0300

    all parts completed

--- Object-OrientedProgramming.py
+++ Object-OrientedProgramming.py
@@ -1,3 +1,4 @@
+import random
 from tabulate import tabulate
 """
 # part 1
@@ -69,18 +70,9 @@ car1 = Car("ABC-123", 142, 60, 2000)
 car1.drive(1.5)
 print(f"travelled distance: {car1.travelled_dist}")
 """
-import random
+
 
 # part 4
-"""Now we will program a car race. The travelled distance of a new car is initialized as zero. At the beginning of 
-the main program, create a list that consists of 10 car objects created using a loop. The maximum speed of each new 
-car is a random value between 100 km/h and 200 km/h. The registration numbers are created as follows: "ABC-1", 
-"ABC-2" and so on. Now the race begins. One per every hour of the race, the following operations are performed: 
-
-The speed of each car is changed so that the change in speed is a random value between -10 km/h and +15 km/h. This is 
-done using the accelerate method. Each car is made to drive for one hour. This is done with the drive method. """
-
-
 def find_max_distance(car_list):
     max_distance = 0
     for i in car_list:
