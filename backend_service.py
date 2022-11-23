48c13f9d4e242d586b8c6e1c4a733345b9eb3857
amerilain <amerilain@gmail.com>
2022-11-23 21:24:33 +0200

    part 1

--- backend_service.py
+++ backend_service.py
@@ -1,5 +1,28 @@
-from flask import Flask, requests
+from flask import Flask, request
 
 app = Flask(__name__)
-@app.route('/prime')
-def prime():
+
+
+@app.route('/prime_number')
+def prime_number():
+    args = request.args
+    number = int(args.get("number"))
+
+    if number > 2:
+        for n in range(2, number):
+            if (number % n) == 0:
+                answer = 'false'
+                break
+        else:
+            answer = 'true'
+    else:
+        answer = 'false'
+    response = {
+        "number": number,
+        "isPrime": answer
+    }
+    return response
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
