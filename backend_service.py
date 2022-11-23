243c901f02225b3d80c8649c36eb3662899020ab
amerilain <amerilain@gmail.com>
2022-11-23 21:34:05 +0200

    part 1

--- backend_service.py
+++ backend_service.py
@@ -1,27 +1,38 @@
-from flask import Flask, request
+from flask import Flask, request, Response
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/prime_number')
 def prime_number():
-    args = request.args
-    number = int(args.get("number"))
+    try:
+        args = request.args
+        number = int(args.get("number"))
 
-    if number > 2:
-        for n in range(2, number):
-            if (number % n) == 0:
-                answer = 'false'
-                break
+        if number > 2:
+            for n in range(2, number):
+                if (number % n) == 0:
+                    answer = 'false'
+                    break
+            else:
+                answer = 'true'
         else:
-            answer = 'true'
-    else:
-        answer = 'false'
-    response = {
-        "number": number,
-        "isPrime": answer
-    }
-    return response
+            answer = 'false'
+        response = {
+            "number": number,
+            "isPrime": answer
+        }
+        return response
+
+    except ValueError:
+        response = {
+            "message": "Invalid number as entered",
+            "status": 400
+        }
+        json_response = json.dumps(response)
+        http_response = Response(response=json_response, status=400, mimetype="application/json")
+        return http_response
 
 
 if __name__ == '__main__':
